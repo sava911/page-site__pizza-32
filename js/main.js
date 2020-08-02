6401d988340b1e6a49fd692e83156e5056fe34d2
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-02 05:07:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//вызов библиотеки wow.js
+//new WOW().init();
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       100,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+
+/*
+1-й способ анимации модалки
+$('a.open-modal').click(function(event) {
+  $(this).modal({
+    fadeDuration: 250
+  });
+  return false;
+});*/
